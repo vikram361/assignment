40e7ba8d5d35629ed02f5e7e5595c7ec624d318a
vikram361 <vikram361@users.noreply.github.com>
2020-06-23 19:54:47 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const menuIcon = document.getElementById("menu-icon");
+const slideoutMenu = document.getElementById("slideout-menu");
+
+
+menuIcon.addEventListener('click', function () {
+    if (slideoutMenu.style.opacity == "1") {
+        slideoutMenu.style.opacity = '0';
+        slideoutMenu.style.pointerEvents = 'none';
+    } else {
+        slideoutMenu.style.opacity = '1';
+        slideoutMenu.style.pointerEvents = 'auto';
+    }
+});
